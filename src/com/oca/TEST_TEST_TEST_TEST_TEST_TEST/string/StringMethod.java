6eb7b5f0eae4c074ac2a2822a8a731b282ba9fbb
Quanaqt <quanaqtzip4@gmail.com>
2025-03-19 23:34:16 +0700

    call by value

--- src/com/oca/TEST_TEST_TEST_TEST_TEST_TEST/string/StringMethod.java
+++ src/com/oca/TEST_TEST_TEST_TEST_TEST_TEST/string/StringMethod.java
@@ -9,6 +9,8 @@ package com.oca.TEST_TEST_TEST_TEST_TEST_TEST.string;
 * * */
 
 
+import java.util.Arrays;
+
 public class StringMethod {
     public static void main(String[] args) {
         Number s = new Integer(1);
@@ -22,6 +24,43 @@ public class StringMethod {
         Object t = new Integer(11);
         int r1 = ((Integer) t).intValue()/ 3;
         System.out.println(r1);
+
+        StringBuilder sb = new StringBuilder("hehe");
+        ObjectR r = new ObjectR();
+        int []is = new int[]{3,4,5};
+        changeString("ss", sb, r, is);
+        System.out.println(sb);
+        System.out.println(r);
+        System.out.println(is[0]);
     }
 
+    public static void changeString(String s, StringBuilder sb, ObjectR r, int[] is) {
+        s = null;
+        sb.append("quan");
+        sb = null;
+//        sb = sb.append("s");
+        r.name = "Asd1";
+        r = null;
+
+        is[0] = 15;
+        is[2] = 3;
+        is = null;
+
+        r = new ObjectR();
+        r.name = "asdkjhasd";
+    }
+}
+// java dung co che callByValue de goi ham: truyen vao 1 ban copy X cua cac object goc G de thuc thi tren do, neu thay doi thuoc tinh thi ca 2 se bi thay doi
+// , nhung neu ben trong ham ma object bi gan sang 1 doi tuong khac K, thi X se tro sang 1 vung nho khac, G van la G, luc nay rieng biet han vs X, khong bi gan sang K
+// truoc khi gan sang K, neu X thay doi gia tri thuoc tinh thi ca X va G deu dc thay doi nhu nhau do van con tro chung vao 1 vung nho data
+
+class ObjectR {
+        String name = "ObjectR";
+
+    @Override
+    public String toString() {
+        return "ObjectR{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
