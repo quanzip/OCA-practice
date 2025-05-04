bc1dd595a908bd17231d15165ce0d9442aad87df
quanzip <quanaqtzip4@gmail.com>
2025-05-04 18:41:28 +0700

    add

--- src/com/oca/TEST_TEST_TEST_TEST_TEST_TEST/bigtest2/Bai7.java
+++ src/com/oca/TEST_TEST_TEST_TEST_TEST_TEST/bigtest2/Bai7.java
@@ -1,5 +1,8 @@
 package com.oca.TEST_TEST_TEST_TEST_TEST_TEST.bigtest2;
-
+/*
+* thú tự ưu tiên gọi các phương thức nạp chồng overloading: Nếu param là 1 số Integer thì thứ tự các PT nạp chồng có param như sau:  INteger/int -> long ->  Integer.../int.../long...
+*
+* */
 public class Bai7 {
     public static void main(String[] args) {
         String[] f = new String[2];
